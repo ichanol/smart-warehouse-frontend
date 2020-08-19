8e74a76f906f1c06e7f09646bfa6d8106d51dec1
Natchanon Suapech <natchanon.sua@gmail.com>
2020-08-19 16:42:50 +0700

    Change Pathname

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css';
 
 import Login from './pages/login'
-import IEProduct from './pages/Import-Export';
+import IEProduct from './pages/ImportExport';
 
 function App() {
   return (
