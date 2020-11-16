85e8f4cd8ced567054e43e63dfe6b9a5fa938309
oscar <chanatip.ras@mail.kmutt.ac.th>
2020-11-16 18:09:21 +0700

    fix: getTransactionList function

--- src/Pages/Transaction.js
+++ src/Pages/Transaction.js
@@ -1,47 +1,68 @@
+import { Container, FilterBlock, Input } from '../Pages/TransactionStyle'
+import { CrossIcon, SearchIcon } from '../components/Icon' //
 import {
-  Container,
-  FilterBlock,
-  Input,
-} from '../Pages/TransactionStyle'
-import { CrossIcon, SearchIcon } from '../components/Icon'
-import React, { useEffect, useState } from 'react'
-
-import { Datepicker } from '../components/Datepicker'
-import {
-  TransactionTable,
+  DropDown,
+  FilterIcon,
+  Pagination,
+  ResponsiveTable,
+  SearchBox,
 } from '../components'
-import { atomState } from '../Atoms'
-import { getRequest } from '../Services'
-import { useRecoilValue } from 'recoil'
+import React, { useEffect, useRef, useState } from 'react'
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
 
-function Transaction() {
-  const [data, setData] = useState([])
+import { Datepicker } from '../components/Datepicker' //
+import { TransactionTable } from '../components' //
+import { atomState } from '../Atoms'
+import { debounce } from 'lodash'
+import { request } from '../Services'
+import { useHistory } from 'react-router-dom'
+
+const Transaction = () => {
+  const history = useHistory()
+
+  const setToastState = useSetRecoilState(atomState.toastState)
+  const userState = useRecoilValue(atomState.userState)
+
+  const scrollRef = useRef([])
+  const searchRef = useRef()
+  const dropDownRef = useRef()
+
+  const [numberPerPage, setNumberPerPage] = useState(20)
+  const [activePage, setActivePage] = useState(1)
+  const [totalPage, setTotalPage] = useState([])
+  const [totalRecord, setTotalRecord] = useState(null)
+  const [refreshFlag, setRefreshFlag] = useState(false)
+  const [search, setSearch] = useState({ status: false, data: [], text: '' })
+  const [sort, setSort] = useState({ column: null, desc: false })
+  const [filter, setFilter] = useState({
+    available: false,
+    notAvailable: false,
+  })
+
+  const [transactionData, setTransactionData] = useState([])
+
   const [date, setDate] = useState({ start: '', end: '' })
   const [keyword, setKeyword] = useState('')
-  const [sort, setSort] = useState({ column: '', isSortUp: false, sortDirection: '' }) // DESC === Down || ASC === UP
   const [amount, setAmount] = useState({ start: '', end: '' })
-
   const [selected, setSelected] = useState('')
   const [open, setOpen] = useState(false)
 
-  const TOKEN = useRecoilValue(atomState.userState)
-
-  const params = {
-    startdate: `${date.start}`,
-    enddate: `${date.end}`,
-    column: `${sort.column}`,
-    sort: `${sort.sortDirection}`,
-    action: `${selected}`,
-    keyword: `${keyword}`,
-    start: `${amount.start}`,
-    end: `${amount.end}`,
+  const queryParams = {
+    column: sort.column,
+    desc: sort.desc,
+    search: null,
+    amount: null,
+    balance: null,
+    status: null,
+    action: null,
+    page: activePage,
+    numberPerPage,
   }
 
   const handleSelect = (option) => {
     setSelected(option)
     setOpen(!open)
   }
-  const toggle = () => setOpen(!open)
 
   const handleSort = (name) => {
     if (sort.column === name) {
@@ -59,29 +80,91 @@ function Transaction() {
     }
   }
 
-  const transactionList = () => {
-    /* getRequest('/product-transaction', params, TOKEN.accessToken, 'get')
-      .then(res => setData(res.result))
-      .catch(err => {
-        throw err
-      }) */
+  const getTransactionList = async () => {
+    try {
+      const { success, totalPages, totalRecords, result } = await request(
+        '/product-transaction',
+        queryParams,
+        userState.accessToken,
+        'get',
+      )
+      if (success) {
+        const temp = []
+        for (let i = 1; i <= totalPages; i = i + 1) {
+          temp.push(i)
+        }
+        setTotalPage(temp)
+        setTotalRecord(totalRecords)
+        setTransactionData(result)
+      }
+      console.log(result)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  const setStart = (dateStart) => setDate({ start: dateStart, end: date.end })
+  const onSearchInputChange = debounce(
+    (text) => setSearch({ ...search, text }),
+    300,
+  )
 
-  const setEnd = (dateEnd) => setDate({ start: date.start, end: dateEnd })
+  const onSearchBoxBlur = () =>
+    setSearch((oldState) => ({
+      ...oldState,
+      status: false,
+      data: [],
+    }))
 
-  const search = (event) => setKeyword(event.target.value)
+  const onSearchBoxFocus = () =>
+    setSearch((oldState) => ({ ...oldState, status: true, data: [] }))
+
+  const onClearSearchBox = () => {
+    searchRef.current.value = ''
+    setSearch({ ...search, text: '' })
+    setRefreshFlag(!refreshFlag)
+  }
+
+  const onSubmitSearch = (event) => {
+    if (event.key === 'Enter') {
+      setActivePage(1)
+      setSearch({ ...search, status: false })
+      setRefreshFlag(!refreshFlag)
+    }
+  }
+
+  const onCheckBoxChange = (filterType) => {
+    const filterOptions = { ...filter }
+    filterOptions[filterType] = !filterOptions[filterType]
+    setFilter(filterOptions)
+    setActivePage(1)
+  }
+
+  // const toggle = () => setOpen(!open)
+  // const setStart = (dateStart) => setDate({ start: dateStart, end: date.end })
+
+  // const setEnd = (dateEnd) => setDate({ start: date.start, end: dateEnd })
+
+  // const search = (event) => setKeyword(event.target.value)
 
   useEffect(() => {
-    transactionList()
+    getTransactionList()
   }, [sort, keyword, selected, date, amount])
 
+  const onChangeNumberPerPage = (number, primaryIndex) => {
+    dropDownRef.current.scrollTop = 40 * (primaryIndex - 1)
+    setNumberPerPage(number)
+    setActivePage(1)
+  }
+
+  const itemPerPageList = [20, 40, 60, 80, 100]
 
   return (
     <Container>
       <div className='header'>
         <span>Transaction</span>
+      </div>
+
+      {/* <div className='header'>
         <FilterBlock>
           <div className='filter'>
             <div className='search'>
@@ -91,9 +174,15 @@ function Transaction() {
                 value={keyword}
                 onChange={search}
               />
-              {keyword && <i className='clearIcon' onClick={() => setKeyword('')}><CrossIcon /></i>}
+              {keyword && (
+                <i className='clearIcon' onClick={() => setKeyword('')}>
+                  <CrossIcon />
+                </i>
+              )}
 
-              <div className='searchIcon'><SearchIcon /></div>
+              <div className='searchIcon'>
+                <SearchIcon />
+              </div>
             </div>
             <div className='amount-wrap'>
               <div className='amount'>
@@ -109,7 +198,13 @@ function Transaction() {
                     })
                   }}
                 />
-                {amount.start && <div className='amount-start' onClick={() => setAmount({ start: '', end: amount.end })}><CrossIcon /></div>}
+                {amount.start && (
+                  <div
+                    className='amount-start'
+                    onClick={() => setAmount({ start: '', end: amount.end })}>
+                    <CrossIcon />
+                  </div>
+                )}
                 <Input
                   placeholder='Max amount'
                   className='input-amount-max'
@@ -122,35 +217,114 @@ function Transaction() {
                     })
                   }}
                 />
-                {amount.end && <div className='amount-end' onClick={() => setAmount({ start: amount.start, end: '' })}><CrossIcon /></div>}
+                {amount.end && (
+                  <div
+                    className='amount-end'
+                    onClick={() => setAmount({ start: amount.start, end: '' })}>
+                    <CrossIcon />
+                  </div>
+                )}
               </div>
             </div>
             <div>
-              <Datepicker
-                date={date}
-                setStart={setStart}
-                setEnd={setEnd}
-              />
+              <Datepicker date={date} setStart={setStart} setEnd={setEnd} />
             </div>
-            <div>
-              {/* <ActionsDropdown
-                handleSelect={handleSelect}
-                selected={selected}
-                handleToggle={toggle}
-                open={open}
-              /> */}
-            </div>
+            <div />
           </div>
         </FilterBlock>
-      </div>
+      </div> */}
       <div className='content'>
-        <TransactionTable
-          data={data}
-          handleSort={handleSort}
-          sort={sort}
-        />
+        {/* <TransactionTable data={[]} handleSort={handleSort} sort={sort} /> */}
+        <div className='tools-bar-wrapper'>
+          <div className='tools-bar'>
+            <SearchBox
+              ref={searchRef}
+              onSearchInputChange={onSearchInputChange}
+              onSubmitSearch={onSubmitSearch}
+              onSearchBoxBlur={onSearchBoxBlur}
+              onSearchBoxFocus={onSearchBoxFocus}
+              onClearSearchBox={onClearSearchBox}
+              text={search.text}
+              data={search.data}
+              status={search.status}
+              field='username'
+            />
+          </div>
+          <div className='tools-bar'>
+            <div className='filter'>
+              <div className='filter-button'>
+                <FilterIcon width={30} />
+                <div className='filter-options'>
+                  <div className='options'>
+                    <div className='options-name'>
+                      <span>Status</span>
+                    </div>
+                    <div className='option-actions'>
+                      <div className='checkbox'>
+                        <label className='custom-checkbox'>
+                          <input
+                            type='checkbox'
+                            checked={filter.available}
+                            onChange={() => onCheckBoxChange('available')}
+                          />
+                          <span className='box' />
+                        </label>
+                        <span className='title'>Available</span>
+                      </div>
+                      <div className='checkbox'>
+                        <label className='custom-checkbox'>
+                          <input
+                            type='checkbox'
+                            checked={filter.notAvailable}
+                            onChange={() => onCheckBoxChange('notAvailable')}
+                          />
+                          <span className='box' />
+                        </label>
+                        <span className='title'>Not Available</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='item-per-page-placeholder'>
+              <span>Shows: </span>
+            </div>
+            <DropDown
+              ref={dropDownRef}
+              selectedValue={numberPerPage}
+              choices={itemPerPageList}
+              onSelect={onChangeNumberPerPage}
+            />
+            <div
+              className='create-new-button'
+              onClick={() => history.push('/user-management/create')}>
+              Create
+            </div>
+          </div>
+        </div>
+        {transactionData.length &&
+          transactionData.map((value, index) => {
+            return (
+              <>
+                <label className='transaction-list'>
+                  <div>
+                    {value.reference_number}, {value.action_name},{' '}
+                    {value.created_at}, {value.detail}, {value.status_value},{' '}
+                    {value.username}
+                  </div>
+                  <input type='checkbox' />
+                  <div className='expand'>
+                    {transactionData[index].data.map((subValue, subIndex) => {
+                      return <li>{subValue.product_id}</li>
+                    })}
+                  </div>
+                </label>
+              </>
+            )
+          })}
       </div>
-    </Container >
+    </Container>
   )
 }
 
--- src/Pages/TransactionStyle.js
+++ src/Pages/TransactionStyle.js
@@ -1,129 +1,286 @@
 import { COLORS, FONT } from '../Constant'
+
 import styled from 'styled-components'
 
 const Container = styled.div`
   height: 100%;
-  padding: 20px;
+  padding: 25px;
   display: flex;
   flex-direction: column;
   overflow: auto;
-  background-color: ${COLORS.gray[200]};
-
+  background-color: ${COLORS.gray[300]};
   .header {
-    height: 100px;
+    height: 35px;
+    align-items: center;
     display: flex;
-    flex-direction: column;
-    margin-bottom: 20px;
+    margin-bottom: 25px;
     font-weight: bold;
     letter-spacing: 1px;
   }
-
-  .header span {
+  .header > span {
     font-size: ${FONT.xl};
+    margin-left: 25px;
   }
-
   .content {
     flex: 1;
     display: flex;
+    padding: 0 25px 25px 25px;
     flex-direction: column;
-    background-color: ${COLORS.natural.white};
-    padding: 0px;
-    min-width: 100%;
-    overflow: auto;
-    filter: blur(${({ blur }) => blur ? 10 : 0}px);
-    position: relative;  
+    position: relative;
   }
-`
-
-const FilterBlock = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  padding: 20px 0;
-
+  .tools-bar-wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+  .tools-bar {
+    height: 40px;
+    display: flex;
+    position: relative;
+  }
+  .tools-bar:not(:first-child) {
+    margin-top: 12px;
+  }
   .filter {
     display: flex;
-    flex-direction: row; 
-    width: 100%;
-
-    
+    flex: 1;
+    max-width: 40px;
   }
-  .amount-wrap { 
-    width: 20%; 
-    padding: 0 20px;
-
-    .amount {
-      display: flex;
-      flex-direction: row;
-      position: relative; 
-
-      input::-webkit-outer-spin-button,
-      input::-webkit-inner-spin-button {
-        -webkit-appearance: none;
-      }
-      }
-
-      .amount-start {
-        position: absolute;
-        top: 30%;
-        left: 35%;
-      }
-
-      .amount-end {
-        position: absolute;
-        top: 30%;
-        left: 85%;
-      }
-
-      .input-amount-min {
-        border-right: 1px solid ${COLORS.gray[500]};
-      }
-
-      .input-amount-max {
-        border-left: 1px solid ${COLORS.gray[500]};
-      }
-  }
-  .action {
-    display: flex; 
-    flex-direction: column;
-  }
-  .button {
-  display: flex;
-  flex-direction: row;
-  position: relative;
-  top: 15px;
-  }
-  .search {
+  .filter-button {
+    background-color: ${COLORS.natural.white};
+    width: 40px;
     display: flex;
+    justify-content: center;
     align-items: center;
+    font-size: ${FONT.l};
+    border-radius: 8px;
+    cursor: pointer;
     position: relative;
-    width: 30%;
-    height: 0%;
-    
+  }
+  .filter-button:hover > .filter-options {
+    display: flex;
+  }
+  .filter-options {
+    display: none;
+    flex-direction: column;
+    position: absolute;
+    width: 300px;
+    left: 0;
+    top: 40px;
+    z-index: 1;
+    box-shadow: 0 5px 15px 0 rgba(0, 0, 0, 0.125),
+      0 -5px 15px 0 rgba(255, 255, 255, 0.35);
+  }
+  .checkbox {
+    align-items: center;
+    display: flex;
+  }
+  .checkbox:not(:first-child) {
+    margin-top: 12px;
+  }
+  .custom-checkbox input[type='checkbox'] {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+  .custom-checkbox input[type='checkbox']:checked + .box {
+    background-color: ${COLORS.blue[400]};
+    border-color: ${COLORS.blue[400]};
+  }
+  .custom-checkbox input[type='checkbox']:checked + .box::after {
+    display: block;
+  }
+  .custom-checkbox {
+    display: flex;
+    position: relative;
+    width: 18px;
+    height: 18px;
+    justify-content: center;
+    align-items: center;
+  }
+  .box {
+    border-radius: 4px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border: 1px solid ${COLORS.gray[400]};
+  }
+  .box::after {
+    display: none;
+    content: '';
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -60%) rotate(45deg);
+    width: 5px;
+    height: 10px;
+    border: solid ${COLORS.natural.white};
+    border-width: 0 3px 3px 0;
+  }
+  .options {
+    display: flex;
+    flex: 1;
+    background-color: ${COLORS.natural.white};
+    padding: 20px;
+  }
+  .options-name {
+    display: flex;
+    flex: 1;
+    max-width: 75px;
+  }
+  .options-name span {
+    color: ${COLORS.gray[700]};
+    font-size: ${FONT.l};
+  }
+  .option-actions {
+    min-height: 5%;
+    padding: 0 15px;
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
+  .checkbox .title {
+    margin-left: 8px;
+    color: ${COLORS.gray[700]};
+    font-size: ${FONT.l};
+  }
+  .item-per-page-placeholder {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 12px;
+  }
+  .items-per-page {
+    display: flex;
+    flex: 1;
+    background-color: ${COLORS.natural.white};
+    align-items: center;
+    justify-content: center;
+    max-width: 80px;
+    border-radius: 8px;
+    cursor: pointer;
+    position: relative;
+  }
+  .items-per-page input {
+    height: 0;
+    width: 0;
+  }
+  .show-item-per-page {
+    position: absolute;
+  }
+  .items-per-page input:checked + .choice-container {
+    overflow-y: auto;
+    height: 120px;
+    width: 100%;
+    position: absolute;
+    left: 0;
+    z-index: 1;
+    display: block;
+    border-radius: 8px;
+    background-color: ${COLORS.natural.white};
+    box-shadow: 0 5px 15px 0 rgba(0, 0, 0, 0.125),
+      0 -5px 15px 0 rgba(255, 255, 255, 0.35);
+  }
+  .items-per-page .choice-container {
+    border-radius: 0;
+    height: 0px;
+    width: 100%;
+    overflow: hidden;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    transition: all 0.3s ease-in-out;
+  }
+  .item-per-page-choice {
+    min-height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-top: 1px solid ${COLORS.gray[200]};
+  }
+  .display-choice-container {
+    background-color: ${COLORS.red[400]};
+    width: 200%;
+    height: 185px;
+    position: absolute;
+    left: 0;
+    z-index: 1;
+  }
 
-    .searchIcon {
-      position: absolute;
-      top: 30%;
-      left: 90%;
+  .number-of-items-indicator {
+    margin-top: 12px;
+    color: ${COLORS.gray[600]};
+  }
+
+  .create-new-button {
+    width: 125px;
+    height: 100%;
+    background-color: ${COLORS.green[500]};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+    position: absolute;
+    right: 0;
+    color: ${COLORS.natural.white};
+    letter-spacing: 0.5px;
+    cursor: pointer;
+  }
+
+  /* width */
+  .search-suggest::-webkit-scrollbar,
+  .choice-container::-webkit-scrollbar {
+    width: 0px;
+  }
+
+  /* Track */
+  .search-suggest::-webkit-scrollbar-track,
+  .choice-container::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+
+  /* Handle */
+  .search-suggest::-webkit-scrollbar-thumb,
+  .choice-container::-webkit-scrollbar-thumb {
+    background: #888;
+  }
+
+  /* Handle on hover */
+  .search-suggest::-webkit-scrollbar-thumb:hover,
+  .choice-container::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+
+  .transaction-list {
+    position: relative;
+    width: 100%;
+    min-height: 70px;
+
+    margin: 20px 0;
+
+    background-color: red;
+
+    input[type='checkbox'] {
+      width: 0;
+      height: 0;
     }
-    
-    .clearIcon {
+
+    .expand {
       position: absolute;
-      top: 30%;
-      left: 80%;
+      top: -100%;
+      width: 100%;
+      height: 0;
+      overflow: hidden;
+      background-color: pink;
+
+      transition: all 0.2 ease-in-out;
+    }
+
+    input:checked + .expand {
+      height: initial;
     }
   }
 `
 
-const Input = styled.input`
-  width: 100%;
-  height: 50px;
-  padding-left: 10px;
-  outline: 0;
-  border: 0;
-  outline: 0;
-  font-size: ${FONT.l};
-`
-
-export { Container, FilterBlock, Input }
+export { Container }
